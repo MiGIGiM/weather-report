052f7ebf0fce5931ccdb3fbab4b8e27745ecadd8
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-01-24 18:03:48 -0600

    🔧 Adding custom background to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'default': "url('/src/assets/img/default.svg')",
+        'warm': "url('/src/assets/img/warm.svg')",
+        'cold': "url('/src/assets/img/cold.svg')",
+      }
+    },
   },
   plugins: [],
 }
